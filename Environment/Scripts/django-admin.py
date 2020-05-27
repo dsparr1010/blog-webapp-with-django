4f26dc2ce7e0ff0ed91a63e99b46867d8bcc9ccf
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-05-27 14:10:02 -0500

    init

---
+++ Environment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\bootcamp\practice\python\blog-webapp-with-django\environment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
