e580501a0056c66bc236caba61f79744f5510602
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-05-27 17:31:17 -0500

    register Post model

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,33 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
 # Called dictionaries in python, equivalent to objects in JS
-posts = [
-    {
-        'author': 'Deb Diesel',
-        'title': 'Blog post 1',
-        'content': 'first post content',
-        'date_posted': 'May 27th, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'second post content',
-        'date_posted': 'March 27th, 2020'
-    },
-    {
-        'author': 'Rhaegar Targaryen',
-        'title': 'Blog post 3',
-        'content': 'third post content',
-        'date_posted': 'May 2nd, 2020'
-    }
-]
 
 #function to handle Home route
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
