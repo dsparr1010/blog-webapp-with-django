f7ebb8296a12ff3656cd914e85907fee3bae3a17
dsparr <dsparr1010@gmail.com>
2020-06-16 14:52:07 -0500

    user profiles and model to handle media/pictures

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # path to directory where we'd like django to store uploaded media files. For performance reasons, these files are stored on the file system and not in the database.
+
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
