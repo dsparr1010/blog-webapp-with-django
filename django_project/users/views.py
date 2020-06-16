e8720856f1163896e88a7ac0029d00890c8eea7e
dsparr <dsparr1010@gmail.com>
2020-06-16 13:32:36 -0500

    login/out and protected profile routes

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create registration form so users can log into site
 # user register form is a class built-in to Django, just need to import it
@@ -12,11 +14,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created. You are now able to login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form' : form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 # Messages available from imported messages:
 
